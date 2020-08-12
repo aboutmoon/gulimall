f441488b689a1f639b3efd536cf68f9c7b78af12
biao.he@kaiostech.com <biao.he@kaiostech.com>
2020-08-12 14:04:47 +0800

    first commit

---
+++ gulimall-ware/src/test/java/com/hb/gulimall/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.gulimall.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
